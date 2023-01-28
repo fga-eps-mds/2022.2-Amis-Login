a401561f6858fb0cb784088b1b65c9e3c25896dc
KalebeLopes <calbkalebe@gmail.com>
2023-01-28 04:39:28 -0300

    (#110): importando rotas na main - https://github.com/fga-eps-mds/2022.2-Amis-Doc/issues/110
    Co-authored-by: Thiago Luiz <thiago.luiz20000@gmail.com>

---
+++ src/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routers
+from login.router import router as login_router
+
+from config import settings
+
+app.include_router(login_router)
+
+@app.get('/')
+async def hello_world():
+    return {
+        "db_type": settings.db_type,
+        "db_url": settings.db_url,
+    }
