3a6948ce63209fcb583b1f3b891dc375d251a818
Gustave Persijn <gpersijn@outlook.com>
2023-06-03 10:58:44 -0300

    #62 - Adicionando rotas de login para estudante
    Co-authored-by: CauaMatheus <caua10000@gmail.com>
    Co-authored-by: caio-lelis <cayolelisdf@gmail.com>

--- src/interfaces/controllers/user_controller.py
+++ src/interfaces/controllers/user_controller.py
@@ -7,6 +7,7 @@ from infrastructure.repositories.tokens_repository import TokensRepository
 from fastapi import APIRouter, Form, Header, HTTPException, status
 
 from interfaces.controllers import socialWorkersService
+from interfaces.controllers import studentService
 
 router = APIRouter(
     prefix='/login',
@@ -15,7 +16,7 @@ router = APIRouter(
 )
 
 
-@router.post("/")
+@router.post("/socialWorker/")
 async def login(username: str = Form(...), password: str = Form(...)):
     access_token, refresh_token = socialWorkersService.login(
         username=username, password=password)
@@ -27,14 +28,14 @@ async def login(username: str = Form(...), password: str = Form(...)):
     }
 
 
-@router.get("/token", status_code=201)
+@router.get("/socialWorker/token", status_code=201)
 async def verificarToken(authorization: str = Header(...)):
     socialWorker = socialWorkersService.verifyToken(authorization)
     socialWorker.senha = None
     return socialWorker
 
 
-@router.post("/token/refresh", status_code=201)
+@router.post("/socialWorker/token/refresh", status_code=201)
 async def refreshToken(refresh_token: str = Header(...)):
     tokens = socialWorkersService.refreshSession(refresh_token=refresh_token)
     if tokens:
@@ -47,8 +48,44 @@ async def refreshToken(refresh_token: str = Header(...)):
     raise HTTPException(401, "Not Allowed")
 
 
-@router.post("/logout")
+@router.post("/socialWorker/logout")
 def logout(refresh_token: str = Header(...)):
     socialWorkersService.delete_refresh_token(refresh_token)
+
+    return {"message": "Logout realizado com sucesso"}
+
+
+@router.post("/student/")
+async def login(username: str = Form(...), password: str = Form(...)):
+    access_token, refresh_token = studentService.login(
+        username=username, password=password)
+
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer",
+    }
+
+@router.get("/student/token", status_code=201)
+async def verificarToken(authorization: str = Header(...)):
+    student = studentService.verifyToken(authorization)
+    student.senha = None
+    return student
+
+@router.post("/student/token/refresh", status_code=201)
+async def refreshToken(refresh_token: str = Header(...)):
+    tokens = studentService.refreshSession(refresh_token=refresh_token)
+    if tokens:
+        return {
+            "access_token": tokens[0],
+            "refresh_token": tokens[1],
+            "token_type": "bearer",
+        }
+
+    raise HTTPException(401, "Not Allowed")
+
+@router.post("/student/logout")
+def logout(refresh_token: str = Header(...)):
+    studentService.delete_refresh_token(refresh_token)
 
     return {"message": "Logout realizado com sucesso"}
